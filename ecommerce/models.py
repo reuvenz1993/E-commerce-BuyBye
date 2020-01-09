9f98b10296904325dacc243da2a635a844f080b8
reuvenz1993 <rovenroven1@gmail.com>
2020-01-09 04:39:16 +0200

    a

---
+++ ecommerce/models.py
@@ -0,0 +1,80 @@
+from blog import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+# By inheriting the UserMixin we get access to a lot of built-in attributes
+# which we will be able to call in our views!
+# is_authenticated()
+# is_active()
+# is_anonymous()
+# get_id()
+
+
+# The user_loader decorator allows flask-login to load the current user
+# and grab their id.
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+
+    # Create a table in the db
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True , nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    photo = db.Column(db.String(64), default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        # https://stackoverflow.com/questions/23432478/flask-generate-password-hash-not-constant-output
+        return check_password_hash(self.password_hash,password)
+
+    def check_if_username_free(self):
+        if User.query.filter_by(username=self.username).first() is not None:
+            return False
+        else:
+            return True
+
+    def check_if_email_free(self):
+        if User.query.filter_by(email=self.email).first() is not None:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        return f'username : {self.username} , email : {self.email} , password : {self.password_hash} ,photo = {self.photo}  '
+
+    def update_photo(self,photo):
+        self.photo = photo
+
+        def update(self, email, username):
+            self.email = email
+            self.username = username
+
+
+class Post(db.Model, UserMixin):
+    # Create a table in the db
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(2048), nullable=False)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id') , nullable=False)
+
+    def __init__(self, user_id, title , content):
+        self.user_id = user_id
+        self.title = title
+        self.content = content
+        self.time = datetime.utcnow()
+        
+    def __repr__(self):
+        return f'by user : {self.username} , post : {self.post}  '
---
+++ ecommerce/views.py
@@ -0,0 +1,118 @@
+from blog import app,db
+from flask import render_template, redirect, request, url_for, flash,abort
+from flask_login import login_user,login_required,logout_user, current_user
+from blog.forms import LoginForm , SignupForm, AddPost , UpdateForm , UpdatePhoto
+from blog.models import User , Post
+import secrets
+import os
+from PIL import Image
+
+def addpost( userid , title , content):
+    new_post = Post(userid , title, content)
+    db.session.add(new_post)
+    db.session.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    loginform = LoginForm()
+    signupform = SignupForm()
+    error = []
+    msg = ''
+    if loginform.login.data and loginform.validate_on_submit():
+        print ("login")
+        logged_in_user = User.query.filter_by(username=loginform.username.data).first()
+        if ( logged_in_user is not None and logged_in_user.check_password(loginform.password.data) ) :
+            print (logged_in_user)
+            to_remember = loginform.remember.data
+            login_user(logged_in_user , remember = to_remember)
+            return redirect(url_for('home'))
+        else:
+            error.append('Invalid username or password')
+            return render_template('index.html' , loginform = loginform , signupform = signupform , error = error )
+
+    if signupform.signup.data and signupform.validate_on_submit():
+        print('signup')
+        register_user = User(email=signupform.email.data,
+                    username=signupform.username.data,
+                    password=signupform.password.data)
+        db.session.add(register_user)
+        try:
+            db.session.commit()
+            msg = 'register complete'
+        except:
+            db.session.rollback()
+            error.append('register failed')
+            if not register_user.check_if_username_free() :
+                error.append('username already exists')
+            if not register_user.check_if_email_free() :
+                error.append('email already exists')
+
+    loginform.reset()
+    signupform.reset()
+    return render_template('index.html' , loginform = loginform , signupform = signupform , error = error , msg=msg )
+
+
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    msg = []
+    addpost_form= AddPost()
+    if addpost_form.addpost.data:
+        addpost(current_user.id , addpost_form.data['title'], addpost_form.data['content'])
+        msg.append('Post added')
+        print ('added a post')
+        addpost_form.reset()
+
+    
+    
+    return render_template('home.html' , addpost_form=addpost_form , msg=msg)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect (url_for('index'))
+
+
+@app.route('/account' , methods=['GET', 'POST'])
+@login_required
+def account():
+    user = User.query.filter_by(username=current_user.username).first_or_404()
+    posts = Post.query.filter_by(user_id=current_user.id).all()
+    email = user.email
+    updateform = UpdateForm()
+    addpost_form= AddPost()
+    updatephoto_form= UpdatePhoto()
+    updateform.email.data = user.email
+    profile_pic = url_for('static', filename='profile_pics/' + user.photo )
+    if updatephoto_form.photo.data :
+        picture_file = save_picture(updatephoto_form.photo.data)
+        user.photo = picture_file
+        db.session.commit()
+        return redirect(url_for('account'))
+    return render_template('account.html', user=user, posts=posts , email=email , updateform=updateform , addpost_form=addpost_form , updatephoto_form=updatephoto_form , profile_pic=profile_pic)
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics' , picture_fn )
+    output_size = (125 , 125)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(picture_path)
+    return picture_fn
+
+@app.route('/home2', methods=['GET', 'POST'])
+@login_required
+def home2():
+    return render_template('home2.html')
+
+
+@app.route('/index2', methods=['GET', 'POST'])
+@login_required
+def index2():
+    return render_template('index2.html')
