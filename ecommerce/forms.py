f87e65659aec8d03f5397f5ba65b3b03f451fe42
reuvenz1993 <rovenroven1@gmail.com>
2020-02-03 11:56:41 +0200

    del old master

---
+++ ecommerce/forms.py
@@ -0,0 +1,48 @@
+from flask_dance.utils import first
+from flask_wtf import FlaskForm
+from wtforms import (IntegerField, PasswordField, StringField, SubmitField, BooleanField ,TextAreaField,
+                     ValidationError , FileField)
+from wtforms.validators import DataRequired, Email, EqualTo
+from ecommerce.models import User
+from ecommerce import db
+
+
+class SignupForm(FlaskForm):
+    email = StringField('Enter your email : ' , validators=[DataRequired() ,Email() ])
+    username = StringField('Enter Username : ' , validators=[DataRequired()] )
+    password = PasswordField('Enter Password: ',validators=[DataRequired()] )
+    signup = SubmitField("Sign up")
+
+
+    def reset(self):
+        self.email.data = ""
+        self.username.data = ""
+        self.password.data = ""
+
+class LoginForm(FlaskForm):
+    username = StringField('Enter Username : ' , validators=[DataRequired()] )
+    password = PasswordField('Enter Password: ', validators=[DataRequired()] )
+    remember = BooleanField('Remember Me')
+    login = SubmitField("Log in")
+
+    def reset(self):
+        self.username.data = ""
+        self.password.data = ""
+
+class AddPost(FlaskForm):
+    title = StringField('Enter Title' , validators=[DataRequired()])
+    content = TextAreaField('Enter Username : ' , validators=[DataRequired()] )
+    addpost = SubmitField("Add Post")
+    
+    def reset(self):
+        self.title.data = ""
+        self.content.data = ""
+
+class UpdateForm(FlaskForm):
+    email = StringField('Enter your email : ' , validators=[DataRequired() ,Email() ])
+    password = PasswordField('Enter Password: ',validators=[DataRequired()] )
+    update = SubmitField("update")
+    
+class UpdatePhoto(FlaskForm):
+    photo = FileField()
+    Upload = SubmitField("update")
