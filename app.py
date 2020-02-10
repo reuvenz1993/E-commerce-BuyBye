d7f496ee8a91a121f180b0824f6aa7d9f5b6203c
reuvenz1993 <rovenroven1@gmail.com>
2020-02-10 07:48:02 +0200

    a

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import ecommerce.views
 import ecommerce.api
 
-
+web: gunicorn app:app
 if __name__ == '__main__':
     app.run(debug=True)
