de55eaa812c030d0411d5cc5c50f95c69a9378e7
reuvenz1993 <rovenroven1@gmail.com>
2020-02-11 19:20:53 +0200

    a

--- ecommerce/static/js/product/product.js
+++ ecommerce/static/js/product/product.js
@@ -2,15 +2,11 @@ qty = parseInt($('#qty').val());
 page = 1
 $(document).ready(function ()
 {
-
-    setTimeout(init,500);
-
     function init()
     {
         handlers();
-        order_or_cart();
-        buy_now()
-
+        buy_now_checkout();
+        buy_now();
     };
 
     function handlers()
@@ -23,13 +19,17 @@ $(document).ready(function ()
             qty = $('#qty').val();
             total_price = qty * product.price;
             $('#total').val(total_price);
-
-
-
         });
 
 
         $('#add_to_cart').click( () => {
+
+            if (typeof current_user === 'undefined' || current_user === null)
+            {
+                $('#buyer_login_modal').modal('show');
+                return;
+            }
+
             data = { 'type' : 'cart' , 'product_id' : product.id , 'qty' : qty };
 
             $.ajax({
@@ -69,15 +69,15 @@ $(document).ready(function ()
 
     };
 
-    function order_or_cart()
-{   
+    function buy_now_checkout()
+    {
     console.log('order_or_cart run');
     $('#checkout').click( (e) => {
 
         console.log(e)
         data = { 'type' : 'buy' , 'product_id' : product.id , 'qty' : qty , 'buyer_message' : $('#buyer_message').val() };
         console.log('order_or_cart')
-        
+
         $.ajax({
             type: "POST",
             url: '/buy_now_or_cart',
@@ -93,32 +93,20 @@ $(document).ready(function ()
                 $('#qty').prop( 'disabled' , 'true' );
                 alert = $('<div></div>').html(temp.product_name +' Ordered successfully - Order id :' + "<a href='/account?focus_order="+temp.order_id+"'>" + temp.order_id + "</a>").addClass('alert alert-success text-center').prependTo('#cont_product');
                 }});
-        
         });
-};
+    };
 
 
     function buy_now()
     {
-
-
-
-        if (typeof current_user === 'undefined' || current_user === null)
-        {
-            //$("#buy_now , #add_to_cart").attr("disabled", true);
-            $("#buy_now , #add_to_cart").click(()=>
-            {
-                $('#buyer_login_modal').modal('show');
-            })
-        } else
-        {
-            //$('#customer_name').val(current_user.name);
-            //$('#order_address').text(current_user.address);
-        };
-
         $("#buy_now").click(function (e)
         {
             e.preventDefault();
+            if (typeof current_user === 'undefined' || current_user === null)
+            {
+                $('#buyer_login_modal').modal('show');
+                return;
+            }
 
             $('#edit').click(function()
             {
@@ -147,12 +135,9 @@ $(document).ready(function ()
                 $('#order_unit_price').text(product.price);
                 $('#order_total').text(total_price);
             });
-
-
-
         });
-
     };
 
+    init();
 });
 
